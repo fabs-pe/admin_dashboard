556c558a3534935debc054e551297ee65939eb4f
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-03-12 20:09:51 +1000

    wip cmdk

---
+++ dashboard/src/app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AccountToggle } from './AccountToggle'
+import { Search } from './Search'
+
+export const Sidebar = () => {
+  return (
+    <div>
+        <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+            <AccountToggle />
+            <Search />
+        </div>
+
+        {/* TODO */}
+    </div>
+  )
+}
