df9cab122f97470b129afe10cfcd5b2b55aec1e1
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-03-21 08:46:03 +1000

    wip: start of cards

--- dashboard/src/app/components/Dashboard/Grid.tsx
+++ dashboard/src/app/components/Dashboard/Grid.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { StatCards } from './StatCards'
+import { ActiveGraph } from './ActiveGraph'
 
 export const Grid = () => {
   return (
-    <div>Grid</div>
+    <div className='px-4 grid gap-3 grid-cols-12'>
+        <StatCards />
+        <ActiveGraph />
+    </div>
   )
 }
