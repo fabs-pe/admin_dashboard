5d92dd9be18b058c557d01ee5a2f7e0a44f0d3db
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-03-21 21:47:21 +1000

    WIP: Add recharts

--- dashboard/src/app/components/Dashboard/Grid.tsx
+++ dashboard/src/app/components/Dashboard/Grid.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StatCards } from './StatCards'
 import { ActiveGraph } from './ActiveGraph'
+import { UsageRadar } from './UsageRadar'
 
 export const Grid = () => {
   return (
     <div className='px-4 grid gap-3 grid-cols-12'>
         <StatCards />
         <ActiveGraph />
+        <UsageRadar />
     </div>
   )
 }
